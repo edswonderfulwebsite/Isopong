ec3fe2d24d320dda159dd72e87b16e8bf99491b7
edward <erlkonig10@gmail.com>
2025-12-26 18:30:34 -0500

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,25 +1,22 @@
 class Camera {
-    constructor(canvas){
-        this.pos = {x: 0, y: 0};
+    constructor(canvas) {
+        this.pos = { x: 0, y: 0 };
         this.zoom = 1;
         this.canvas = canvas;
         this.easing = 0.05;
-        this.target = null; // usually the ball
+        this.target = null;
     }
 
-    update(){
-        if(!this.target) return;
-
-        // Smooth follow
+    update() {
+        if (!this.target) return;
         this.pos.x += (this.target.pos.x - this.pos.x) * this.easing;
         this.pos.y += (this.target.pos.y - this.pos.y) * this.easing;
 
-        // Optional: zoom based on ball height
         const targetZoom = 1 - (this.target.z / this.target.maxZ) * 0.1;
         this.zoom += (targetZoom - this.zoom) * this.easing;
     }
 
-    apply(ctx){
-        ctx.setTransform(this.zoom, 0, 0, this.zoom, this.canvas.width/2 - this.pos.x*this.zoom, this.canvas.height/2 - this.pos.y*this.zoom);
+    apply(ctx) {
+        ctx.setTransform(this.zoom, 0, 0, this.zoom, this.canvas.width / 2 - this.pos.x * this.zoom, this.canvas.height / 2 - this.pos.y * this.zoom);
     }
 }
