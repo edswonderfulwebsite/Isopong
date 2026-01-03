888d1bd9e8db7586639f0649fef15551386c45d0
edward (creator of isopong game) <erlkonig10@gmail.com>
2026-01-03 10:18:33 -0500

    Update paddle.js

--- js/paddle.js
+++ js/paddle.js
@@ -2,41 +2,57 @@ class Paddle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.z = 12;
 
-    this.width = 14;
-    this.height = 64;
+    this.vx = 0;
+    this.vy = 0;
 
     this.prevX = x;
     this.prevY = y;
 
-    this.vx = 0;
-    this.vy = 0;
+    this.width = 14;
+    this.height = 70;
+
+    this.stiffness = 80;   // spring strength
+    this.damping = 0.85;   // velocity decay
 
     this.sprite = new Image();
     this.sprite.src = "assets/sprites/paddle.png";
   }
 
-  update(mouseX, mouseY, dt) {
-    // Track velocity for physics transfer
+  update(cursorX, cursorY, dt) {
     this.prevX = this.x;
     this.prevY = this.y;
 
-    // Paddle follows cursor directly.
-    this.x = mouseX;
-    this.y = mouseY;
+    // spring force
+    const fx = (cursorX - this.x) * this.stiffness;
+    const fy = (cursorY - this.y) * this.stiffness;
 
-    this.vx = (this.x - this.prevX) / dt;
-    this.vy = (this.y - this.prevY) / dt;
+    this.vx += fx * dt;
+    this.vy += fy * dt;
+
+    this.vx *= this.damping;
+    this.vy *= this.damping;
+
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
   }
 
-  draw(ctx, isoSkew) {
+  draw(ctx) {
     ctx.drawImage(
       this.sprite,
       this.x - this.width / 2,
-      this.y * isoSkew - this.z - this.height / 2,
+      this.y - this.height / 2,
       this.width,
       this.height
     );
+  }
+
+  getHitbox() {
+    return {
+      x: this.x,
+      y: this.y,
+      w: this.width,
+      h: this.height
+    };
   }
 }
