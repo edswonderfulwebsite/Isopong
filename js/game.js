3d7f33f1c5eacbf7fa282c81c1e1e46f7d2a1d9c
edward (creator of isopong game) <erlkonig10@gmail.com>
2026-01-03 10:20:47 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,38 +1,11 @@
 class Game {
   constructor(canvas) {
-    this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
 
-    this.width = canvas.width;
-    this.height = canvas.height;
+    this.mouse = { x: canvas.width / 2, y: canvas.height / 2 };
 
-    this.isoSkew = 0.5;
-
-    // Table centered to canvas
-    const tableWidth = 360;
-    const tableHeight = 200;
-
-    this.table = {
-      x: this.width / 2,
-      y: this.height / 2 + 40,
-
-      left: this.width / 2 - tableWidth / 2,
-      right: this.width / 2 + tableWidth / 2,
-      top: this.height / 2 - tableHeight / 2,
-      bottom: this.height / 2 + tableHeight / 2,
-
-      isoSkew: this.isoSkew
-    };
-
-    // Paddle starts behind table
-    this.paddle = new Paddle(
-      this.table.left - 30,
-      this.table.y
-    );
-
-    this.ball = new Ball(this.paddle, this.table);
-
-    this.mouse = { x: this.paddle.x, y: this.paddle.y };
+    this.paddle = new Paddle(this.mouse.x, this.mouse.y);
+    this.ball = new Ball(this.paddle);
 
     canvas.addEventListener("mousemove", e => {
       const rect = canvas.getBoundingClientRect();
@@ -46,43 +19,20 @@ class Game {
       }
     });
 
-    this.lastTime = performance.now();
-  }
-
-  update(dt) {
-    this.paddle.update(this.mouse.x, this.mouse.y, dt);
-    this.ball.update(dt, this.paddle);
-  }
-
-  drawTable() {
-    const t = this.table;
-    const ctx = this.ctx;
-
-    ctx.fillStyle = "#2e8b57";
-    ctx.beginPath();
-    ctx.moveTo(t.left, t.top * this.isoSkew);
-    ctx.lineTo(t.right, t.top * this.isoSkew);
-    ctx.lineTo(t.right, t.bottom * this.isoSkew);
-    ctx.lineTo(t.left, t.bottom * this.isoSkew);
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  draw() {
-    this.ctx.clearRect(0, 0, this.width, this.height);
-
-    this.drawTable();
-    this.ball.draw(this.ctx);
-    this.paddle.draw(this.ctx, this.isoSkew);
+    this.last = performance.now();
   }
 
   loop = () => {
     const now = performance.now();
-    const dt = (now - this.lastTime) / 1000;
-    this.lastTime = now;
+    const dt = (now - this.last) / 1000;
+    this.last = now;
 
-    this.update(dt);
-    this.draw();
+    this.paddle.update(this.mouse.x, this.mouse.y, dt);
+    this.ball.update(dt, this.paddle);
+
+    this.ctx.clearRect(0, 0, 960, 540);
+    this.ball.draw(this.ctx);
+    this.paddle.draw(this.ctx);
 
     requestAnimationFrame(this.loop);
   };
